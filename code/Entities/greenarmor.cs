4b5e482c0d40301ec2b5238d697e5cfe33f8d4e0
Baik90 <88790023+Baik90@users.noreply.github.com>
2022-05-07 10:56:22 +0200

    add Armor

---
+++ code/Entities/greenarmor.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Hammer;
+using System.ComponentModel;
+
+/// <summary>
+/// Gives 100 Armor
+/// </summary>
+[Library( "dmc_greenarmor", Title = "Green Armor" )]
+[EditorModel( "models/items/armor/armor_green.vmdl" )]
+[Display( Name = "Green Armor" ), Category( "Armor" ), Icon( "security" )]
+partial class ArmorGreen : ModelEntity, IRespawnableEntity
+{
+	public static readonly Model WorldModel = Model.Load( "models/items/armor/armor_green.vmdl" );
+
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		Model = WorldModel;
+
+		PhysicsEnabled = true;
+		UsePhysicsCollision = true;
+
+		CollisionGroup = CollisionGroup.Weapon;
+		SetInteractsAs( CollisionLayer.Debris );
+	}
+
+	public override void StartTouch( Entity other )
+	{
+		base.StartTouch( other );
+
+		if ( other is not DeathmatchPlayer player ) return;
+		if ( player.Armour >= 100 ) return;
+
+		var newhealth = player.Armour + 100;
+
+		newhealth = newhealth.Clamp( 0, 100 );
+
+		player.Armour = newhealth;
+
+		Sound.FromWorld( "dm.item_battery", Position );
+		PickupFeed.OnPickup( To.Single( player ), $"+100 Armour" );
+
+		ItemRespawn.Taken( this );
+		Delete();
+	}
+}
